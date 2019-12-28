3a484ece2afc3b05f1f0ebf346ffc6da89360708
Travis CI <travis@travis-ci.org>
2019-12-28 17:11:10 +0800

    update and test,

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,6 +4,6 @@ docker build -t logickee/kicad-automation-scripts .
 docker build -t logickee/kicad Dockerfile.kicad
 
 docker push logickee/kicad-automation-scripts
-docker push logickee/kicad Dockerfile.kicad
+docker push logickee/kicad
 
 # docker image rm $(docker image ls -qa)
