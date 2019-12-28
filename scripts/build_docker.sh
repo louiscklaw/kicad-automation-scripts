7c8bc29d4ee9b8712d3df1fd00d4d5037944b5b1
louiscklaw <louiscklaw@gmail.com>
2019-12-28 23:28:32 +0800

    init commit for relocate dockerfile,

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-docker build -t logickee/kicad-automation-scripts .
-docker build -t logickee/kicad Dockerfile.kicad
+# docker build -t logickee/kicad-base -f Dockerfile.kicad-base dockerfiles
+docker build -t logickee/kicad-base -f ./dockerfiles/Dockerfile.kicad-base ./dockerfiles
+docker build -t logickee/kicad-automation-scripts -f ./dockerfiles/Dockerfile.kicad-automation ./dockerfiles
 
+docker push logickee/kicad-base
 docker push logickee/kicad-automation-scripts
-docker push logickee/kicad
 
 # docker image rm $(docker image ls -qa)
