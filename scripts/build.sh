5ab28ed7a05bd4524063154468773c6f26897654
Travis CI <travis@travis-ci.org>
2019-12-29 17:04:13 +0800

    init commit and test,

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,31 +4,8 @@
 echo 'Run schematic ERC'
 sh scripts/test_run_erc.sh
 sh scripts/test_gen_svg.sh
-
-# docker run --rm -t \
-#   -v $PWD/test/test_proj1:/kicad-project \
-#   -v $PWD/src:/usr/lib/python2.7/dist-packages/kicadAutomation \
-#   logickee/kicad-automation-scripts \
-#   python -m kicadAutomation.eeschema.schematic run_erc /kicad-project/test.sch /kicad-project/ERC_output
-# ls -l test/test_proj1/ERC_output
-
-# echo 'Export a schematic to PDF or SVG'
-# sh scripts/gen_svg.sh
-# ls -l test/test_proj1/SVG_output
-
-# echo 'Run layout DRC:'
-# docker run --rm -it \
-#   -v $PWD/test/test_proj1:/kicad-project \
-#   -v $PWD/src:/usr/lib/python2.7/dist-packages/kicadAutomation \
-#   logickee/kicad-automation-scripts \
-#   python -m kicadAutomation.pcbnew_automation.run_drc /kicad-project/test.kicad_pcb /kicad-project/DRC_output
-
-# echo 'Generate a zip file with gerber files for PCB manufacuring:'
-# docker run --rm -t \
-#   -v $PWD/test/test_proj1:/kicad-project \
-#   -v $PWD/src:/usr/lib/python2.7/dist-packages/kicadAutomation \
-#   logickee/kicad-automation-scripts \
-#   python -m kicadAutomation.pcbnew_automation.plot /kicad-project/test.kicad_pcb PLOT_output
+sh scripts/test_run_layout_drc.sh
+sh scripts/test_gen_zip_ger_file.sh
 
 # echo 'Generate a pdf with the layout layers and drill map file:'
 # docker run --rm -t \
---
+++ scripts/test_run_layout_drc.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo 'Run layout DRC:'
+echo "schematic directory: $2"
+echo "output directory: $3"
+
+
+docker run --rm -it \
+  -v $PWD/src:/usr/lib/python3/dist-packages/kicadAutomation \
+  -v $1:/kicad-project \
+  logickee/kicad-automation-scripts \
+  python -m kicadAutomation.pcbnew_automation.run_drc /kicad-project/test.kicad_pcb /kicad-project/DRC_output
+
+sudo chown 1000:1000 -R $1/DRC_output
+
+echo 'gen_svg done'
+
+
+# echo 'Run layout DRC:'
+# docker run --rm -it \
+#   -v $PWD/test/test_proj1:/kicad-project \
+#   -v $PWD/src:/usr/lib/python2.7/dist-packages/kicadAutomation \
+#   logickee/kicad-automation-scripts \
+#   python -m kicadAutomation.pcbnew_automation.run_drc /kicad-project/test.kicad_pcb /kicad-project/DRC_output
