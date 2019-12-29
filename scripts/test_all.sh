cb0d6775c18ddfa3833958c3bbcb10de148cb710
Travis CI <travis@travis-ci.org>
2019-12-29 19:30:00 +0800

    update,.

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -8,3 +8,11 @@ rm -rf $(find ./test -type d -name "*_output")
 
 bash ./scripts/test1.sh | tee ./scripts/test1.log
 bash ./scripts/test2.sh | tee ./scripts/test2.log
+
+
+echo 'collecting error from result'
+echo '---'
+
+grep -i error scripts/test1.log scripts/test2.log | tee scripts/this_run_result.log
+
+echo '---'
